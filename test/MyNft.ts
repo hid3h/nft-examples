26d68840c176f7f6d505c278c4e5e17266dc24fd
hid3h <razyetts@hotmail.co.jp>
2023-01-02 16:24:51 +0900

    MyNftのテスト動くとこまで

---
+++ test/MyNft.ts
@@ -0,0 +1,30 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("MyNft", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployMyNftFixture() {
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const MyNft = await ethers.getContractFactory("MyNft");
+    const myNft = await MyNft.deploy();
+
+    return { myNft, owner, otherAccount };
+  }
+
+  describe("MyNfts", function () {
+    describe("Hello", function () {
+      it("Hello", async function () {
+        const { myNft } = await loadFixture(deployMyNftFixture);
+
+        expect(await myNft.hello()).to.equal(
+          "Hello"
+        );
+      });
+    });
+  });
+});
