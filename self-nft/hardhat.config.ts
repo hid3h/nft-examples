fdc71f129e57ec9db927cf313f05b41feb13504f
hid3h <razyetts@hotmail.co.jp>
2023-03-22 11:39:39 +0900

    修正

--- self-nft/hardhat.config.ts
+++ self-nft/hardhat.config.ts
@@ -3,7 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 require('dotenv').config();
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
-const PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
