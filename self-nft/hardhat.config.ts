d6b8f9bffac71b9dd1055f1acfadd50897bfcfc1
hid3h <razyetts@hotmail.co.jp>
2023-03-26 16:31:56 +0900

    mainnetでのverifyのために

--- self-nft/hardhat.config.ts
+++ self-nft/hardhat.config.ts
@@ -1,6 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-require('dotenv').config();
+require("dotenv").config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
@@ -9,18 +9,19 @@ const config: HardhatUserConfig = {
   networks: {
     mainnet: {
       url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.MAINNET_ALCHEMY_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""]
+      accounts: [process.env.PRIVATE_KEY || ""],
     },
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${process.env.GOERLI_ALCHEMY_API_KEY}`,
-      accounts: [process.env.TESTNET_PRIVATE_KEY || ""]
-    }
+      accounts: [process.env.TESTNET_PRIVATE_KEY || ""],
+    },
   },
   etherscan: {
     apiKey: {
-      goerli: process.env.GOERLI_ETHERSCAN_API_KEY || "",
-    }
-  }
+      goerli: process.env.ETHERSCAN_API_KEY || "",
+      mainnet: process.env.ETHERSCAN_API_KEY || "",
+    },
+  },
 };
 
 export default config;
