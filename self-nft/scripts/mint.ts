7d63f5e372de3e9483669c94e0b07bd6f6c68747
hid3h <razyetts@hotmail.co.jp>
2023-03-26 16:05:03 +0900

    mint修正

--- self-nft/scripts/mint.ts
+++ self-nft/scripts/mint.ts
@@ -1,26 +1,29 @@
+// npx hardhat run scripts/mint.ts --network goerli
 import { ethers } from "hardhat";
-require('dotenv').config();
+require("dotenv").config();
 
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
-const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS || "";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const contract = require("../artifacts/contracts/MyNft.sol/MyNft.json");
 
-const contract = require("../artifacts/contracts/MyNft.sol/MyNft.json")
-
-const provider = new ethers.providers.AlchemyProvider("mainnet", ALCHEMY_API_KEY)
-const signer = new ethers.Wallet(PRIVATE_KEY, provider)
+const provider = ethers.provider;
+const signer = provider.getSigner();
 const abi = contract.abi;
 
-const myNftContract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+const myNftContract = new ethers.Contract(
+  process.env.GOERLI_CONTRACT_ADDRESS || "",
+  abi,
+  signer
+);
 
 async function main() {
-  const to = signer.address
-  const tokenId = 1
-  const nftTxn = await myNftContract.safeMint(to, tokenId)
+  const to = await signer.getAddress();
+  const tokenId = 1;
+  const nftTxn = await myNftContract.safeMint(to, tokenId);
 
-  await nftTxn.wait()
+  await nftTxn.wait();
 
-  console.log(`NFT Minted! Check it out at: https://etherscan.io/tx/${nftTxn.hash}`)
+  console.log(
+    `NFT Minted! Check it out at: https://etherscan.io/tx/${nftTxn.hash}`
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
