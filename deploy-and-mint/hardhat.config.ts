01d6a1b034960f5fd45072191aaa0228305ec85c
hid3h <razyetts@hotmail.co.jp>
2024-02-16 13:20:42 +0900

    コントラクトをデプロイできました

--- deploy-and-mint/hardhat.config.ts
+++ deploy-and-mint/hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.MUMBAI_ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
 };
 
 export default config;
